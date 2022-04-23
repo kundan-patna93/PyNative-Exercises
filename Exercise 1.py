34ac2f5c0a1e034614de06b920aa948dd3d3f362
kundan kumar <kundan.patna93@gmail.com>
2022-04-23 16:27:20 +0530

    Delete Exercise 1.py

--- Exercise 1.py
+++
@@ -1,9 +0,0 @@
-class Vehical:
-    def __init__(self, s, m):
-        self.speed=s
-        self.mileage=m
-        print("speed:", self.speed)
-        print("Mileage: ",self.mileage)
-
-
-v=Vehical(120,35)
