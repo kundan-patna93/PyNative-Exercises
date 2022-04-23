4b44db3d37f29ab9af6a63e7f32e8c9c5da70e9c
kundan kumar <kundan.patna93@gmail.com>
2022-04-23 16:24:22 +0530

    Add files via upload

---
+++ Exercise 1.py
@@ -0,0 +1,9 @@
+class Vehical:
+    def __init__(self, s, m):
+        self.speed=s
+        self.mileage=m
+        print("speed:", self.speed)
+        print("Mileage: ",self.mileage)
+
+
+v=Vehical(120,35)
